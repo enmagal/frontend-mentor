e5f6e61227ca5042b1f749ef75d8099eddbc755f
enzo-magal <61931855+enzo-magal@users.noreply.github.com>
2024-02-17 21:02:51 +0100

    :sparkles: header

--- frontend-mentor-result/src/App.js
+++ frontend-mentor-result/src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
+import ProjectCards from "./components/ProjectCards";
 import FaqAccordion from "./faq-accordion/FaqAccordion";
 import NewsletterSignUp from "./newsletter-sign-up/NewsletterSignUp";
-
-function MyButton() {
-  return <button>I'm a button</button>;
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fronted Mentor Result</h1>
-        <p>
-          Cette page a pour objectif de regrouper mes résultats de challenge
-          frontend mentor
-        </p>
-        <MyButton />
+        <h1 className="title">
+          Here are my amazing <br /> Frontend Mentor Results{" "}
+        </h1>
+        <div className="buttons">
+          <a className="button" href="bento.me/enzomagal">
+            See my Bento
+          </a>
+          <a className="button" href="bento.me/enzomagal">
+            The projects
+          </a>
+        </div>
       </header>
+      <div className="projects">
+        <h2>Projects</h2>
+        <ProjectCards />
+      </div>
       <FaqAccordion />
       <NewsletterSignUp />
     </div>
---
+++ frontend-mentor-result/src/components/ProjectCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProjectCards(props) {
+  return (
+    <div>
+      <img src="../assets/image/desktop-preview.jpg" alt="project" />
+      <h1>Faq Accordion</h1>
+      <a href="truc.com">See the project</a>
+    </div>
+  );
+}
+
+export default ProjectCards;
