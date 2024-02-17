dd6276fa7ae75434a5c83a5896fe6962e23e3ae5
enzo-magal <61931855+enzo-magal@users.noreply.github.com>
2024-02-17 15:56:52 +0100

    :sparkles: Ending faqAccordion

--- frontend-mentor-result/src/faq-accordion/FaqAccordion.js
+++ frontend-mentor-result/src/faq-accordion/FaqAccordion.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FaqAccordion.css";
 import { ReactComponent as ReactStar } from "./assets/images/icon-star.svg";
 import { ReactComponent as ReactPlus } from "./assets/images/icon-plus.svg";
 import { ReactComponent as ReactMinus } from "./assets/images/icon-minus.svg";
 
-function Question({ question, text, hide }) {
-  if (hide) {
+const data = [
+  {
+    name: "1",
+    content: {
+      question: "What is Frontend Mentor, and how will it help me?",
+      text: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+    },
+  },
+  {
+    name: "2",
+    content: {
+      question: "Is Frontend Mentor free?",
+      text: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+  },
+  {
+    name: "3",
+    content: {
+      question: "Can I use Frontend Mentor projects in my portfolio?",
+      text: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+  },
+  {
+    name: "4",
+    content: {
+      question: "How can I get help if I'm stuck on a challenge?",
+      text: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+    },
+  },
+];
+
+function Question({ name, content, toggle, open }) {
+  if (!open) {
     return (
       <div className="question">
-        <div className="questionHeader">
-          <h2 className="questionTitle">{question}</h2>
+        <div onClick={() => toggle(name)} className="questionHeader">
+          <p className="questionTitle">{content.question}</p>
           <ReactPlus />
         </div>
       </div>
@@ -17,51 +48,42 @@ function Question({ question, text, hide }) {
   }
   return (
     <div className="question">
-      <div className="questionHeader">
-        <h2 className="questionTitle">{question}</h2>
+      <div onClick={() => toggle(name)} className="questionHeader">
+        <p className="questionTitle">{content.question}</p>
         <ReactMinus />
       </div>
-
-      <p className="questionText">{text}</p>
+      <p className="questionText">{content.text}</p>
     </div>
   );
 }
 
 function FaqAccordion(props) {
+  const [openKey, setOpenKey] = useState("1");
+
+  const handleToggle = (key) => {
+    setOpenKey(openKey !== key ? key : null);
+  };
+
   return (
     <div className="faqAccordionSection">
       <div className="faqAccordion">
         <div className="title">
           <ReactStar />
-          <h1>FAQs</h1>
+          <h1 className="faqTitle">FAQs</h1>
         </div>
 
-        <Question
-          question="What is Frontend Mentor, and how will it help me?"
-          text="Frontend Mentor offers realistic coding challenges to help developers improve their 
-        frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-        all levels and ideal for portfolio building."
-          hide={false}
-        />
-        <Question
-          question="Is Frontend Mentor free?"
-          text="Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
-          option providing access to a range of projects suitable for all skill levels."
-          hide={true}
-        />
-        <Question
-          question="Can I use Frontend Mentor projects in my portfolio?"
-          text="Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-          way to showcase your skills to potential employers!"
-          hide={true}
-        />
-        <Question
-          question="How can I get help if I'm stuck on a Frontend Mentor challenge?"
-          text="The best place to get help is inside Frontend Mentor's Discord community. There's a help 
-          channel where you can ask questions and seek support from other community members."
-          hide={true}
-        />
+        {data &&
+          data.map(({ name, content }) => (
+            <Question
+              key={name}
+              name={name}
+              content={content}
+              toggle={handleToggle}
+              open={openKey === name}
+            />
+          ))}
       </div>
+      <div className="greyBackground"></div>
     </div>
   );
 }
