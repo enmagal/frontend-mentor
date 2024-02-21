94b169b17b74963a15825b239731ed7cb42fb6f8
enzo-magal <61931855+enzo-magal@users.noreply.github.com>
2024-02-21 20:14:47 +0100

    continue dev agecalc

--- frontend-mentor-result/src/age-calculator/AgeCalculator.js
+++ frontend-mentor-result/src/age-calculator/AgeCalculator.js
@@ -17,27 +17,82 @@ function TextInput(props) {
   );
 }
 
+function calculateAge(birthDay, birthMonth, birthYear) {
+  const currentDate = new Date();
+  const birthDate = new Date(birthMonth + "/" + birthDay + "/" + birthYear);
+  const Anniversary = new Date(
+    birthMonth + "/" + birthDay + "/" + currentDate.getFullYear()
+  );
+  const day = 0;
+  const month = currentDate.getMonth() + 1;
+  var year = 0;
+  if (currentDate > Anniversary) {
+    year = currentDate.getFullYear() - birthDate.getFullYear();
+  } else {
+    year = currentDate.getFullYear() - birthDate.getFullYear() - 1;
+  }
+  return [day, month, year];
+}
+
 function AgeCalculator(props) {
-  const [year, setYear] = useState(38);
-  const [month, setMonth] = useState(3);
-  const [day, setDay] = useState(26);
+  const [year, setYear] = useState("--");
+  const [month, setMonth] = useState("--");
+  const [day, setDay] = useState("--");
+  const [birthDay, setBirthDay] = useState();
+  const [birthMonth, setBirthMonth] = useState();
+  const [birthYear, setBirthYear] = useState();
+  const handleChangeYear = (event) => {
+    setBirthYear(event.target.value);
+  };
+  const handleChangeMonth = (event) => {
+    setBirthMonth(event.target.value);
+  };
+  const handleChangeDay = (event) => {
+    setBirthDay(event.target.value);
+  };
+  const handleClick = () => {
+    const [daysNb, monthNb, yearNb] = calculateAge(
+      birthDay,
+      birthMonth,
+      birthYear
+    );
+    setDay(daysNb);
+    setMonth(monthNb);
+    setYear(yearNb);
+  };
   return (
     <div className="AgeCalculatorSection">
       <div className="AgeCalculatorCard">
         <div className="AgeForm">
-          <TextInput name="day" placeholder="DD" />
-          <TextInput name="month" placeholder="MM" />
-          <TextInput name="year" placeholder="YYYY" />
+          <TextInput name="day" placeholder="DD" onChange={handleChangeDay} />
+          <TextInput
+            name="month"
+            placeholder="MM"
+            onChange={handleChangeMonth}
+          />
+          <TextInput
+            name="year"
+            placeholder="YYYY"
+            onChange={handleChangeYear}
+          />
         </div>
         <div className="validation">
           <div className="line" />
-          <button className="ArrowButton">
+          <button className="ArrowButton" onClick={handleClick}>
             <ReactArrow />
           </button>
         </div>
-        <h1>{year} years</h1>
-        <h1>{month} months</h1>
-        <h1>{day} days</h1>
+        <div className="AgeCalculatorResult">
+          <h1>
+            <em>{year}</em> years
+          </h1>
+          <h1>
+            <em>{month}</em> months
+          </h1>
+          <h1>
+            <em>{day}</em> days
+          </h1>
+        </div>
       </div>
     </div>
   );
